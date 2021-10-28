3ce49bcea16bcef88702e132b0306fc51fb1f4d7
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2021-10-28 04:03:06 +0300

    Update __init__.py

--- saicinpainting/training/trainers/__init__.py
+++ saicinpainting/training/trainers/__init__.py
@@ -1,6 +1,6 @@
 import logging
 import torch
-from saicinpainting.training.trainers.default import DefaultInpaintingTrainingModule
+from .default import DefaultInpaintingTrainingModule
 
 
 def get_training_model_class(kind):
